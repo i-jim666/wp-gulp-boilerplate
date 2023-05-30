a504645b1d2a978eaa9aff60bdac1bd32dd2e80a
i-jim666 <ishtiaque.jim@gmail.com>
2023-05-30 12:34:44 +0600

    JS files updated

--- dist/js/theme.js
+++ dist/js/theme.js
@@ -2,41 +2,4 @@
 //   e.preventDefault();
 // })
 
-// Carousels
-
-// document.addEventListener( 'DOMContentLoaded', function() {
-
-//   if(document.querySelector('.featured-carousel')){
-//     const featured_carousel = new Splide( '.featured-carousel', {
-//         type   : 'loop',
-//         arrows: false,
-//         pagination: false,
-//         speed: 800,
-//         gap: '48px',
-//         perPage: 5,
-//         perMove: 1,
-//         drag: false,
-
-//         autoScroll: {
-//           speed: 1,
-//           pauseOnHover: false,
-//           pauseOnFocus: false,
-//         },
-
-//         breakpoints: {
-//           600: {
-//             perPage: 4,
-
-//             autoScroll: {
-//               speed: 0.7,
-//               pauseOnHover: false,
-//               pauseOnFocus: false,
-//             },
-//           }
-//         }
-
-//     } );
-//     featured_carousel.mount(window.splide.Extensions);
-//   }
-
-// } );
+console.log('silence is golden');
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,3 +1,5 @@
 // $('a[href="#contact"]').on('click', function(e){
 //   e.preventDefault();
 // })
+
+console.log('silence is golden');
